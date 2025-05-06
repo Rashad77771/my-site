b6fc376609177af250dd7a35684a3f75965a95ef
Rashad77771 <fermanov449@gmail.com>
2025-05-06 03:30:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelector('.cta-button').addEventListener('click', () => {
+    alert('1win-ə xoş gəlmisiniz!');
+});
+document.getElementById('register-btn').addEventListener('click', () => {
+    window.location.href = 'https://clck.ru/3LseqY';
+});
+document.getElementById('login-btn').addEventListener('click', () => {
+    window.location.href = 'https://clck.ru/3LseqY';
+});
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        window.location.href = 'https://clck.ru/3LseqY';
+    });
+});
