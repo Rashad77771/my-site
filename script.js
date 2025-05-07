1fe047682cb79e4d78576e3bed39d97e145bf13f
Rashad77771 <fermanov449@gmail.com>
2025-05-07 19:56:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ document.querySelector('.cta-button').addEventListener('click', () => {
     alert('1win-ə xoş gəlmisiniz!');
 });
 document.getElementById('register-btn').addEventListener('click', () => {
-    window.location.href = 'https://clck.ru/3LseqY';
+    window.location.href = 'https://1wzyuh.com/casino/list?open=register&p=uojt';
 });
 document.getElementById('login-btn').addEventListener('click', () => {
-    window.location.href = 'https://clck.ru/3LseqY';
+    window.location.href = 'https://1wzyuh.com/casino/list?open=register&p=uojt';
 });
 document.querySelectorAll('.nav-link').forEach(link => {
     link.addEventListener('click', (e) => {
         e.preventDefault();
-        window.location.href = 'https://clck.ru/3LseqY';
+        window.location.href = 'https://1wzyuh.com/casino/list?open=register&p=uojt';
     });
 });
