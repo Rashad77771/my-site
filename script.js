9a55b24b16c230bbb6683156b27158897687dd20
Rashad77771 <fermanov449@gmail.com>
2025-08-23 02:57:33 +0400

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ document.querySelector('.cta-button').addEventListener('click', () => {
     alert('1win-ə xoş gəlmisiniz!');
 });
 document.getElementById('register-btn').addEventListener('click', () => {
-    window.location.href = 'https://1wzocn.life/casino/list?open=register&p=ivce';
+    window.location.href = 'https://1wksxo.life/casino/list/4?p=3z7q';
 });
 document.getElementById('login-btn').addEventListener('click', () => {
-    window.location.href = 'https://1wzocn.life/casino/list?open=register&p=ivce';
+    window.location.href = 'https://1wksxo.life/casino/list/4?p=3z7q';
 });
 document.querySelectorAll('.nav-link').forEach(link => {
     link.addEventListener('click', (e) => {
         e.preventDefault();
-        window.location.href = 'https://1wzocn.life/casino/list?open=register&p=ivce';
+        window.location.href = 'https://1wksxo.life/casino/list/4?p=3z7q';
     });
 });
